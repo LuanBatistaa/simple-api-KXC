43e40a1cc240ea0ad08ab857013f0b08594e10d6
Developer 01 <dev1@denjidev.com>
2025-10-02 02:26:42 -0300

    create bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "meu-bucket-state-api"
+    bucket         = "my-bucket-state-api"
     key            = "simple-api/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
