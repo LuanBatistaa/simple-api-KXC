384ca36ce08ea634d02f1ff8ae101df6223c489a
LuanBatistaa <sousaluan3215@gmailo.com>
2025-10-02 10:26:47 -0300

    destroy infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,8 +30,7 @@ module "ecs" {
   desired_count        = 2
   alb_sg_id            = module.alb.alb_sg_id
   image_tag            = var.image_tag
-  depends_on = [module.alb.aws_lb_listener.http]
-
+  depends_on = [module.alb]
 }
 
 module "secrets" {
