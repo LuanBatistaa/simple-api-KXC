40e6b9d702d4a74c9243c7d0158b9937090ae668
LuanBatistaa <sousaluan3215@gmailo.com>
2025-10-03 09:41:22 -0300

    teste dev create group_rule

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -85,25 +85,28 @@ resource "aws_ecs_service" "this" {
   }
 }
 
-resource "aws_iam_policy" "ecs_secrets_policy" {
-  name        = "ecs-secrets-policy"
-  description = "Permite ECS ler secrets específicos"
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
 
-  policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "secretsmanager:GetSecretValue"
-        ],
-        Resource = "arn:aws:secretsmanager:us-east-1:471112586010:secret:my-db-secret-*"
-      }
-    ]
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_secrets" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = aws_iam_policy.ecs_secrets_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
